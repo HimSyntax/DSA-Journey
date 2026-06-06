a2782c0d8540bb2e1c926c7deb43d9c4332f1132
Himanshu Ranjan <btwhimanshuatwork@gmail.com>
2026-06-06 13:43:30 +0530

    Update demo.java

--- demo.java
+++ demo.java
@@ -463,3 +463,52 @@
 //         printFib(a, b, n-2);
 //     }
 // }
+
+
+
+//Print x^n (stack height = n)
+// class demo{
+//     public static int calcPower(int x, int n){
+//         if(n==0){ //base case 1
+//             return 1;
+//         }
+//         if(x==0){ //base case 2
+//             return 0;
+//         }
+//         int xPownm1 = calcPower(x, n-1); //kaam
+//         int xPown = x * xPownm1;
+//         return xPown;
+//     }
+//     public static void main(String[] args) {
+//         int x = 2, n =5;
+//         int ans = calcPower(x,n);
+//         System.out.println(ans);
+//     }
+// }
+
+
+
+//Print x^n (stack height = logn)
+// class demo{
+//     public static int calcPower(int x, int n){
+//         if(n==0){ //base case 1
+//             return 1;
+//         }
+//         if(x==0){ //base case 2
+//             return 0;
+//         }
+
+        //if n is even 
+//         if(n%2==0){
+//             return calcPower(x, n/2) * calcPower(x, n/2);
+//         }
+//         else{ //n is odd
+//             return calcPower(x,n/2) * calcPower(x,n/2)*x;
+//         }
+//     }
+//     public static void main(String[] args) {
+//         int x=2, n=5;
+//         int ans = calcPower(x,n);
+//         System.out.println(ans);
+//     }
+// }
