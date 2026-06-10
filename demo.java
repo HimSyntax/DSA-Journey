69f49842b4d11d1cbf9f105e4e89d07f9b0ea3bf
Himanshu Ranjan <btwhimanshuatwork@gmail.com>
2026-06-10 14:17:54 +0530

    Update demo.java

--- demo.java
+++ demo.java
@@ -669,3 +669,88 @@
 //                 removeDuplicates(str, 0, " ");
 //         }
 // }
+
+
+
+// Print all the subsequences of a string //
+// time complexity = O(2^n) //
+// class demo{
+//         public static void subsequences( String str, int idx, String newString){
+//                 if(idx==str.length()){
+//                         System.out.println(newString);
+//                         return;
+//                 }
+//                 char currChar = str.charAt(idx);
+
+//                 // to be
+//                 subsequences(str, idx+1, newString+currChar);
+
+//                 // or not to be
+//                 subsequences(str, idx+1, newString);
+//         }
+//       public static void main(String[] args) {
+//         String str = "abc";
+//         subsequences(str, 0, "");
+//       }  
+// }
+
+
+// SPECIAL VARIATION OF THE ABOVE QUESTION //
+
+
+// Print all unique subsequences of a string //
+// import java.util.HashSet;
+// class demo{
+//         public static void subsequences(String str, int idx, String newString, HashSet<String>set){
+//                 if(idx==str.length()){
+//                         if(set.contains(newString)){
+//                                 return;
+//                         } else {
+//                                 System.out.println(newString);
+//                                 set.add(newString);                        
+//                                 return;
+//                         }
+//                 }
+
+//                 char currChar = str.charAt(idx);
+                
+//                 subsequences(str, idx+1, newString+currChar, set);
+
+//                 subsequences(str, idx+1, newString, set);
+//         }
+
+//         public static void main(String[] args) {
+//                 String str = "aaa";
+//                 HashSet<String>set = new HashSet<>();
+//                 subsequences(str, 0, "", set);
+//         }
+// }
+
+
+
+// Print keypad combination //
+// time complexity = O(4^n) //
+// class demo{
+//         public static String[] keypad = {".", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tu", "vwx", "yz"};
+
+//         public static void printComb(String str, int idx, String combination){
+//                 if(idx==str.length()){
+//                         System.out.println(combination);
+//                         return;
+//                 }
+//                 char currChar = str.charAt(idx);
+//                 String mapping = keypad[currChar - '0'];
+
+//                 for(int i=0; i<mapping.length(); i++){
+//                         printComb(str, idx+1, combination+mapping.charAt(i));
+//                 }
+//         }
+//         public static void main(String[] args) {
+//                 String str = "4";
+//                 printComb(str, 0, "");
+//                 }
+// }
+
+
+
+/* RECURSION */   // CLASS 3 //
