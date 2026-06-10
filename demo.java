38b633421d9c3d6fd2ef4e5ea5128a563b9e0525
Himanshu Ranjan <btwhimanshuatwork@gmail.com>
2026-06-11 01:11:17 +0530

    Update demo.java

--- demo.java
+++ demo.java
@@ -754,3 +754,139 @@
 
 
 /* RECURSION */   // CLASS 3 //
+
+// Print all permutations of a string //
+// time complexity = O(n!) //
+// class demo{
+//         public static void printPerm(String str, String permutation){
+//                 if(str.length()==0){
+//                         System.out.println(permutation);
+//                         return;
+//                 }
+
+//                 for(int i=0; i<str.length(); i++){
+//                       char currChar = str.charAt(i);
+//                       //"abc"
+//                       String newStr = str.substring(0, i) + str.substring(i+1); 
+//                       printPerm(newStr, permutation + currChar);
+//                 }
+//         }
+//         public static void main(String[] args) {
+//                 String str ="abc";
+//                 printPerm(str, "");
+//         }
+// }
+
+
+
+// Count total paths in  maze to move from (0,0) to (n,n) //
+// dynamic programming approach + backtracking //
+// class demo{
+//         public static int countPaths( int i, int j, int n, int m){
+//                 if(i==n || j==n){
+//                         return 0;
+//                 }
+//                 if(i==n-1 && j==m-1){
+//                         return 1;
+//                 }
+
+//                 // move downwards
+//                 int downPaths = countPaths(i+1, j, n,m);
+
+//                 // move right
+//                 int rightPaths = countPaths(i, j+1, n, m);
+
+//                 return downPaths + rightPaths;
+//         }
+//         public static void main(String[] args) {
+//                 int n = 3, m = 3;
+//                 int totalPaths = countPaths(0,0,n,m);
+//                 System.out.println(totalPaths);
+//         }
+// }
+
+
+
+// Place tiles of size 1xm in a floor of size nxm //
+// class demo{
+//         public static int placeTiles(int n, int m){
+//                 if(n==m){
+//                         return 2;
+//                 }
+
+//                 if(n<m){
+//                         return 1;
+//                 }
+
+//                 // vertically
+//                 int vertPlacements = placeTiles(n-m, m);
+
+//                 // horizontally
+//                 int horPlacements = placeTiles(n-1, m);
+
+//                 return vertPlacements + horPlacements;
+//         }
+//         public static void main(String[] args) {
+//                 int n = 4;
+//                 int m = 2;
+                
+//                 System.out.println(placeTiles(n,m));
+//         }
+// }
+
+
+
+// Find the numbers of ways in which you can invite n people to party, single or in pairs //
+// class demo{
+//         public static int callGuests( int n){
+//                 if(n<=1){
+//                         return 1;
+//                 }
+
+//                 // single
+//                 int ways1 = callGuests(n-1);
+
+//                 // pair
+//                 int ways2 = (n-1) * callGuests(n-2);
+
+//                 return ways1 + ways2;
+//         }
+//         public static void main(String[] args) {
+//                 int n = 4;
+//                 System.out.println(callGuests(n));
+//         }
+// }
+
+
+
+// Print all the subsets of a set of first n natural numbers //
+// time complexity = O(2^n) //
+// import java.util.*;
+// class demo{
+//         public static void printSubset(ArrayList<Integer> subset){
+//                 for(int i=-0; i<subset.size(); i++){
+//                         System.out.print(subset.get(i)+" ");
+//                 }
+//                 System.out.println();
+//         }
+//         public static void findSubsets( int n, ArrayList<Integer> subset){
+
+//                 if(n==0){
+//                         printSubset(subset);
+//                         return;
+//                 }
+
+//                 // add hoga
+//                 subset.add(n);
+//                 findSubsets(n-1, subset);
+
+//                 // add nahi hoga
+//                 subset.remove(subset.size()-1);
+//                 findSubsets(n-1, subset);
+//         }
+//         public static void main(String[] args) {
+//                 int n = 3;
+//                 ArrayList<Integer> subset = new ArrayList<>();
+//                 findSubsets(n, subset);
+//         }
+// }
