131d08ae22f093ed7aed4c4940f1a507c08cd3bc
Himanshu Ranjan <btwhimanshuatwork@gmail.com>
2026-06-10 13:23:41 +0530

    Update demo.java

--- demo.java
+++ demo.java
@@ -512,3 +512,160 @@
 //         System.out.println(ans);
 //     }
 // }
+
+
+
+
+
+/* RECURSION */  // CLASS 2 //
+
+// Tower of Hanoi // 
+
+// O(2^n-1)~O(2^n) // 
+// T(n) = 2T(n-1)+1 // 
+// 2^n-1 T(1)+2^n-2+2^n-3+...
+// x=n-1 //
+
+// class demo{
+//         public static void towerOfHanoi( int n , String src, String helper , String dest){
+//                 if(n==1){
+//                         System.out.println("transfer disk" + n + " from " + src + " to " +dest);
+//                         return;
+//                 }
+//                 towerOfHanoi(n-1, src , dest, helper);
+//                 System.out.println("transfer disk" + n + " from " + src + " to " + dest);
+//                 towerOfHanoi(n-1, helper, src, dest);
+//         }
+//         public static void main(String[] args) {
+//                 int n = 3;
+//                 towerOfHanoi(n, "S", "H", "D");
+//         }
+// }
+
+
+
+// Print a string in reverse //
+// time complexity = O(n) //
+// class demo{
+//         public static void printRev(String str, int idx){
+//                 if(idx==0){
+//                         System.out.println(str.charAt(idx));
+//                         return;
+//                 }
+//                 System.out.println(str.charAt(idx));
+//                 printRev(str, idx-1);
+//         }
+//         public static void main(String[] args) {
+//                 String str = "abcd";
+//                 printRev(str, str.length()-1);
+//         }
+// }
+
+
+
+// Find the 1st & last occurance of an element in string //
+// time complexity = O(n) //
+// class demo{
+//         public static int first = -1;
+//         public static int last = -1;
+
+//         public static void findOccurance( String str, int idx, char element){
+//                 if(idx==str.length()){
+//                         System.out.println(first);
+//                         System.out.println(last);
+//                         return;
+//                 }
+                
+//                 char currChar = str.charAt(idx);
+//                 if(currChar==element){
+//                         if(first == -1){
+//                                 first = idx;
+//                         } else{
+//                                 last = idx;
+//                         }
+//                 }
+//                 findOccurance(str, idx+1, element);
+//         }
+//         public static void main(String[] args) {
+//                 String str = "abaacdaefaah";
+//                 findOccurance(str, 0,'a');
+//         }
+// }
+
+
+
+// Check if an array is sorted( strictly increasing) //
+// time complexity = O(n) //
+// class demo{
+//         public static boolean isSorted( int arr[], int idx){
+//                 if(idx==arr.length-1){
+//                         return true;
+//                 }
+//                 if(arr[idx] < arr[idx+1]){
+//                         //array is sorted till now
+//                         return isSorted(arr, idx+1);
+//                 } else {
+//                         return false;
+
+//                 }
+//         }
+//         public static void main(String[] args) {
+//                 int arr[] = {1,3,4,5};
+//                 System.out.println(isSorted(arr, 0));
+//         }
+// }
+
+
+
+// Move all 'x' to the end of the string //
+// time complexity = O(2n) which is O(n) //
+// class demo{
+//         public static void moveAllX( String str, int idx, int count, String newString){
+//                 if(idx==str.length()){
+//                         for(int i=0; i<count; i++){
+//                                 newString += 'x';
+//                         }
+//                         System.out.println(newString);
+//                         return;
+//                 }
+//                 char currChar = str.charAt(idx);
+//                 if(currChar == 'x'){
+//                         count++;
+//                         moveAllX(str, idx+1, count, newString);
+//                 } else {
+//                         newString += currChar; // newString = newString + currChar
+//                         moveAllX(str, idx+1, count, newString);
+//                 }
+//         }
+//         public static void main(String[] args) {
+//                 String str = "axbcxxd";
+//                 moveAllX(str, 0, 0, "");
+//         }
+// }
+
+
+
+// Remove duplicates in a string //
+// time complexity = O(n) // 
+// class demo{
+//         public static boolean[] map = new boolean[26];
+
+//         public static void removeDuplicates( String str, int idx, String newString){
+//                 if(idx==str.length()){
+//                         System.out.println(newString);
+//                         return;
+//                 }
+//                 char currChar = str.charAt(idx);
+//                 if(map[currChar - 'a']){
+//                         removeDuplicates(str, idx+1, newString);
+//                 } else {
+//                         newString += currChar;
+//                         map[currChar - 'a'] = true;
+//                         removeDuplicates(str, idx+1, newString);
+//                 }
+//         }
+//         public static void main(String[] args) {
+//                 String str = "abbccda";
+//                 removeDuplicates(str, 0, " ");
+//         }
+// }
