2e29f2b6b75e26c20f9d4335ef730d0301e96215
Himanshu Ranjan <btwhimanshuatwork@gmail.com>
2026-06-18 13:51:30 +0530

    Update demo.java

--- demo.java
+++ demo.java
@@ -1091,3 +1091,60 @@
 //                 helper(board, 0,0);
 //         }
 // }
+
+
+
+/* ARRAY LIST  */
+
+// import java.util.ArrayList;
+// import java.util.Collections;
+
+// class demo{
+//         public static void main(String[] args) {
+//                // Integer | Float | String | Boolean //
+//                ArrayList<Integer> list = new ArrayList<Integer>();
+               
+//                // add elements 
+//                list.add(0);
+//                list.add(1);
+//                list.add(2);
+//                list.add(3);
+
+//                System.out.println(list);
+
+//                // get elements
+//                int element = list.get(0);
+               
+//                System.out.println(element);
+
+
+//                // add element in between 
+//                list.add(4,4);
+//                System.out.println(list);
+
+
+//                // set element
+//                list.set(0,5);
+//                System.out.println(list);
+
+//                // delete element 
+//                list.remove(3);
+//                System.out.println(list);
+
+//                // size
+//                int size = list.size();
+//                System.out.println(size);
+
+
+//                // loops in array list
+//                for(int i=0; i<list.size(); i++){
+//                 System.out.print(list.get(i));
+//                }
+//                System.out.println();
+
+
+//                // sorting
+//                Collections.sort(list);
+//                System.out.println(list);
+//         }
+// }
