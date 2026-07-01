540a5abf685b1504741600007b6ee50b5a7568bf
Himanshu Ranjan <btwhimanshuatwork@gmail.com>
2026-07-01 22:52:42 +0530

    Update demo.java

--- demo.java
+++ demo.java
@@ -1170,3 +1170,44 @@
 
 //! HASHSET !//
 
+// import java.util.HashSet;
+// import java.util.Iterator;
+// class demo{
+//         public static void main(String[] args) {
+//                 //creating
+//                 HashSet<Integer>set = new HashSet<>();
+
+//                 //insert
+//                 set.add(1); 
+//                 set.add(2);
+//                 set.add(3);
+//                 set.add(1); //list.add(el)
+
+//                 //size
+//                 System.out.println("size of set is : " + set.size());
+
+//                 //print all elements
+//                 System.out.println(set);
+
+//                 //search-contains
+//                 if(set.contains(1)){
+//                         System.out.println("set contains 1");
+//                 }
+//                 if(!set.contains(6)){
+//                         System.out.println("does not contain");
+//                 }
+
+//                 //delete
+//                 set.remove(1);
+//                 if(!set.contains(1)){
+//                         System.out.println("does not contain 1 - we deleted 1");
+//                 }
+
+//                 //iterator
+//                 Iterator it = set.iterator();
+
+//                 while(it.hasNext()){
+//                         System.out.println(it.next());
+//                 }
+//         }
+// }
