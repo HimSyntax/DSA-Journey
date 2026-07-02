b419a9674c5a4d9040e6d2f801f560a8695d3f92
Himanshu Ranjan <btwhimanshuatwork@gmail.com>
2026-07-02 23:42:29 +0530

    Update demo.java

--- demo.java
+++ demo.java
@@ -1211,3 +1211,65 @@
 //                 }
 //         }
 // }
+
+
+
+// import java.util.HashMap;
+// import java.util.Map;
+// import java.util.Set;
+// class demo{
+//         public static void main(String[] args) {
+//                 //country(key), population(value)
+//                 HashMap<String, Integer> map = new HashMap<>();
+
+//                 //Insertion
+//                 map.put("India", 120);
+//                 map.put("US", 30);
+//                 map.put("China", 150);
+
+//                 System.out.println(map);
+
+//                 map.put("China", 180);
+//                 System.out.println(map);
+
+//                 //Search (LookUp)
+//                 if(map.containsKey("China")){
+//                 System.out.println("present in the map");
+//                 } else {
+//                         System.out.println("not present in the map");
+//                 }
+
+//                 System.out.println(map.get("China")); //key exist
+//                 System.out.println(map.get("Indonesia")); //key doesn't exist
+
+
+//                 //Loops
+
+//                 int arr[] = {12,15,18};
+//                 for( int i=0; i<3; i++){
+//                         System.out.println(arr[i]+ " ");
+//                 } 
+//                 System.out.println();
+
+//                 for(int val : arr){
+//                         System.out.println(val+ " ");
+//                 }
+//                 System.out.println();
+
+//                 //Iteration
+
+//                 for( Map.Entry<String, Integer> e : map.entrySet() ){
+//                         System.out.println(e.getKey());
+//                         System.out.println(e.getValue());
+//                 }
+
+//                 Set<String> keys = map.keySet();
+//                 for( String key : keys){
+//                         System.out.println(key + " " + map.get(key));
+//                 }
+
+//                 //Remove
+//                 map.remove("China");
+//                 System.out.println(map);
+//         }
+// }
