d3aa93da62d769158a2ca70113f91e20f93da67a
Himanshu Ranjan <btwhimanshuatwork@gmail.com>
2026-07-06 22:40:33 +0530

    Update Striver.java

--- Striver.java
+++ Striver.java
@@ -1,8 +1,34 @@
                         // ! STRIVER A.2.Z DSA ! //
 
 
-public class Striver {
+//! BASIC MATHS !//
+
+// Count Digits //
+
+// class Striver{
+//     public static void main(String[] args) {
+//         int num = 1234;
+//         int count = 0;
+
+//         while(num!=0){
+//             count++;
+//             num = num / 10;
+//         }
+//         System.out.println("Total Digits = " + count);
+//     }
+// }
+
+// Reverse a Number //
+
+class Striver{
     public static void main(String[] args) {
-        
+         int num = 12345;
+         int reverse = 0;
+          while(num!=0){
+            int digit = num % 10;
+            reverse = reverse * 10 + digit;
+            num = num/10;
+          }
+          System.out.println("Reversed Number = " + reverse);
     }
 }
