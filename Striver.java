99e56c1dbb546e3879bf4d60ced5a760da13cd82
Himanshu Ranjan <btwhimanshuatwork@gmail.com>
2026-07-23 22:53:56 +0530

    Update Striver.java

--- Striver.java
+++ Striver.java
@@ -169,3 +169,20 @@
 //         printN(N,0);
 //     }
 // }
+
+
+// Factorial of a given number //
+
+// class Striver{
+//     public static int Factorial(int n){
+//         if(n==1){
+//             return 1;
+//         } else {
+//             return n * Factorial(n-1);
+//         }
+//     }
+//     public static void main(String[] args) {
+//         int n = 6;
+//         System.out.println(Factorial(n));
+//     }
+// }
