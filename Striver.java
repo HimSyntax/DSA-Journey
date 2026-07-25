84df47babcc3efb6f73d188472c8bfb70d52f33f
Himanshu Ranjan <btwhimanshuatwork@gmail.com>
2026-07-25 22:13:03 +0530

    Update Striver.java

--- Striver.java
+++ Striver.java
@@ -186,3 +186,27 @@
 //         System.out.println(Factorial(n));
 //     }
 // }
+
+
+// Reverse an Array //
+
+// class Striver {
+//     public static void reverse(int [] arr, int left, int right){
+//         if(left>=right){
+//             return;
+//         }
+//         int temp = arr[left];
+//         arr[left] = arr[right];
+//         arr[right] = temp;
+ 
+//         reverse (arr, left+1, right-1);
+//     }
+//     public static void main(String[] args) {
+//         int [] arr = {1,2,3,4,5};
+//         reverse(arr, 0, arr.length-1);
+
+//         for( int i=0; i<arr.length; i++){
+//             System.out.print(arr[i]+" ");
+//         }
+//     }
+// }
