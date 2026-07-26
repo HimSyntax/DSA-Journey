c565c67430984855078313875a19c9887c90babb
Himanshu Ranjan <btwhimanshuatwork@gmail.com>
2026-07-26 18:34:44 +0530

    Update Striver.java

--- Striver.java
+++ Striver.java
@@ -210,3 +210,25 @@
 //         }
 //     }
 // }
+
+
+
+//! BASIC HASHING !//
+
+
+// Basic Hashing //
+
+// class Striver{
+//     public static void main(String[] args) {
+//         int [] arr = {1,2,1,3,2};
+//         int number = 3;
+//         int count = 0;
+
+//         for(int i=0; i<arr.length; i++){
+//             if(arr[i]==number){
+//                 count++; 
+//             }
+//         }
+//         System.out.println(count);
+//     }
+// }
