f22c1ca9e54914890001d49a160818d506800e88
Himanshu Ranjan <btwhimanshuatwork@gmail.com>
2026-08-11 00:24:57 +0530

    Update Striver.java

--- Striver.java
+++ Striver.java
@@ -377,3 +377,23 @@
 //         System.out.println(sorted);
 //     }
 // }
+
+
+// Check if the Array is Sorted //
+
+// class Striver {
+//     public static void main(String[] args) {
+//         int[]arr = {1,1,2,2,2,3,3};
+
+//         int i = 0;
+//         for(int j=1; j<arr.length; j++){
+//             if(arr[i]!=arr[j]){
+//                 arr[i+1] = arr[j];
+//                 i++;
+//             }
+//         }
+//         for(int k=0; k<=i; k++){
+//             System.out.println(arr[k]);
+//         }
+//     }
+// }
