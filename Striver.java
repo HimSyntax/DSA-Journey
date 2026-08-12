f0bb6e01f6d323e1568ad83a8bf81574af448e89
Himanshu Ranjan <btwhimanshuatwork@gmail.com>
2026-08-13 00:01:46 +0530

    Update Striver.java

--- Striver.java
+++ Striver.java
@@ -415,3 +415,47 @@
 //         }
 //     }
 // }
+
+
+// Left Rotate Array By K Place //
+
+// class Striver {
+    
+//     public static void reverseArray(int[]nums, int start, int end){
+//         while (start<end) { 
+//             int temp = nums[start];
+//             nums[start] = nums[end];
+//             nums[end] = temp;
+//             start++;
+//             end--;
+//         }
+//     }
+
+//     public static int[] rotateArray(int[]nums, int k, String direction){
+//         int n = nums.length;
+//         if(n==0 || k==0){
+//             return nums;
+//         }
+//         k = k%n;
+//         if(direction.equals("Left")){
+//             reverseArray(nums, 0, k-1);
+//             reverseArray(nums,  k, n-1);
+//             reverseArray(nums, 0, n-1);
+//         }
+//         return nums;
+
+//     }
+
+//     public static void main(String[] args) {
+//         int[]nums = {1,2,3,4,5,6,7};
+//         int k =3;
+//         String direction = "Left";
+
+//         int[]result = rotateArray(nums, k, direction);
+
+//         for(int num : result){
+//             System.out.print(num + " ");
+//         }
+
+//     }
+// }
