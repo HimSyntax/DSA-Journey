87fde75a9ba897574f332e68797bbfbb98aa9b4c
Himanshu Ranjan <btwhimanshuatwork@gmail.com>
2026-08-18 03:24:56 +0530

    Update Striver.java

--- Striver.java
+++ Striver.java
@@ -548,3 +548,25 @@
 
 //     }
 // }
+
+
+// !Optimal Solution! //
+
+// class Striver {
+    
+//     public static void main(String[] args) {
+    
+//         int[]arr = {1,2,3,5};
+//         int n = arr.length+1;
+//         int xor = 0;
+
+//         for(int i=0; i<=n; i++){
+//             xor = xor^i;
+//         }
+//         for(int i=0; i<arr.length; i++){
+//             xor = xor^arr[i];
+//         }
+
+//         System.out.println(xor);
+//     }
+// }
