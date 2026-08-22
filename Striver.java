4e429180d7b17c7676a86ab523d04139762e334f
Himanshu Ranjan <btwhimanshuatwork@gmail.com>
2026-08-23 01:13:43 +0530

    Update Striver.java

--- Striver.java
+++ Striver.java
@@ -1,7 +1,4 @@
-                                      // !  D-S-A  ! //
 
-
-// Count Digits //
 
 // class Striver{
 //     public static void main(String[] args) {
@@ -593,4 +590,20 @@
 //         }
 //         System.out.println(max);
 //         }
+// }
+
+
+// Find the number that appears once, and other numbers twice //
+
+// class Striver {
+//     public static void main(String[] args) {
+         
+//         int[]arr = {1,1,2,3,3,4,4};
+//         int xor = 0;
+
+//         for(int i=0; i<arr.length; i++){
+//             xor = xor^arr[i];
+//         }
+//         System.out.println(xor);
+//     }
 // }
