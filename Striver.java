e767991cc2f1000ec5fa97639a557ce012cd265c
Himanshu Ranjan <btwhimanshuatwork@gmail.com>
2026-08-23 00:35:10 +0530

    Update Striver.java

--- Striver.java
+++ Striver.java
@@ -570,3 +570,27 @@
 //         System.out.println(xor);
 //     }
 // }
+
+
+// Maximum Consecutives Ones //
+
+// class Striver {
+//     public static void main(String[] args) {
+        
+//         int[]arr = {1,1,0,0,1,1,1,0,1,1,1,1};
+//         int max = 0;
+//         int cnt = 0;
+        
+//         for(int i=0; i<arr.length; i++){
+//             if(arr[i]==1){
+//                 cnt++;
+
+//                 max = Math.max(max,cnt);
+//             }
+//             else {
+//                 cnt = 0;
+//             }
+//         }
+//         System.out.println(max);
+//         }
+// }
