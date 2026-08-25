93197b6124077ce76ceabd8c83fd0d77d5879ee0
Himanshu Ranjan <btwhimanshuatwork@gmail.com>
2026-08-25 14:23:07 +0530

    Update Striver.java

--- Striver.java
+++ Striver.java
@@ -607,3 +607,69 @@
 //         System.out.println(xor);
 //     }
 // }
+
+
+// Longest subarray with given sum K(positives) //
+
+// class Striver{ // Better //
+//     public static void main(String[] args) {
+        
+//         int[]arr = {1,2,3,1,1,1,1,4,2,3};
+//         int len = 0;
+//         int target = 3;
+
+//         for(int i=0; i<arr.length; i++){
+//             int sum = 0;
+//             for(int j=i; j<arr.length; j++){
+//                 sum+=arr[j];
+//                 if(sum==target){
+//                 len = Math.max(len,j-i+1);
+//             }
+//             }
+//         }
+//         System.out.println(len);
+//     }
+// }
+
+// Optimal //
+
+// import java.util.HashMap;
+// import java.util.Map;
+
+// class Striver {
+//     public static void main(String[] args) {
+
+//         int[] arr = {1, 2, 3, 1, 1, 1, 1, 4, 2, 3};
+//         int k = 3;
+
+//         long sum = 0;
+//         int maxLen = 0;
+
+//         Map<Long, Integer> preSumMap = new HashMap<>();
+
+//         for (int i = 0; i < arr.length; i++) {
+
+//             sum += arr[i];
+
+//             // If subarray starts from index 0
+//             if (sum == k) {
+//                 maxLen = Math.max(maxLen, i + 1);
+//             }
+
+//             long rem = sum - k;
+
+//             // Check if remaining sum exists
+//             if (preSumMap.containsKey(rem)) {
+//                 int len = i - preSumMap.get(rem);
+//                 maxLen = Math.max(maxLen, len);
+//             }
+
+//             // Store prefix sum
+//             if (!preSumMap.containsKey(sum)) {
+//                 preSumMap.put(sum, i);
+//             }
+//         }
+
+//         System.out.println(maxLen);
+//     }
+// }
