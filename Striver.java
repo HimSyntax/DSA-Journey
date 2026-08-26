4c49f88f9083cb191133d9b9c3f102da6b1a01a4
Himanshu Ranjan <btwhimanshuatwork@gmail.com>
2026-08-26 22:30:37 +0530

    Update Striver.java

--- Striver.java
+++ Striver.java
@@ -694,3 +694,45 @@
 //         System.out.println(target);
 //     }
 // }
+
+
+// Sort an array of 0's 1's and 2's //
+
+// class Striver {
+//     public static void main(String[] args) {
+
+//         int[] arr = {0,1,2,0,1,2,1,2,0,0,1};
+//         int cnt0 = 0;
+//         int cnt1 = 0;
+//         int cnt2 = 0;
+
+//         // Count 0s, 1s and 2s
+//         for(int i = 0; i < arr.length; i++) {
+//             if(arr[i] == 0) {
+//                 cnt0++;
+//             }
+//             else if(arr[i] == 1) {
+//                 cnt1++;
+//             }
+//             else {
+//                 cnt2++;
+//             }
+//         }
+//         // Put 0s
+//         for(int i = 0; i < cnt0; i++) {
+//             arr[i] = 0;
+//         }
+//         // Put 1s
+//         for(int i = cnt0; i < cnt0 + cnt1; i++) {
+//             arr[i] = 1;
+//         }
+//         // Put 2s
+//         for(int i = cnt0 + cnt1; i < arr.length; i++) {
+//             arr[i] = 2;
+//         }
+//         // Print array
+//         for(int i = 0; i < arr.length; i++) {
+//             System.out.print(arr[i] + " ");
+//         }
+//     }
+// }
