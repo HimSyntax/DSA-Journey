0d2bc9dd610ebd6feae021e861679d95e5831847
Himanshu Ranjan <btwhimanshuatwork@gmail.com>
2026-08-26 21:52:10 +0530

    Update Striver.java

--- Striver.java
+++ Striver.java
@@ -673,3 +673,24 @@
 //         System.out.println(maxLen);
 //     }
 // }
+
+
+// Two Sum // 
+
+// class Striver {
+//     public static void main(String[] args) {
+        
+//         int[]arr = {1,2,3,5,4,6,3};
+//         int target = 7;
+//         int n = arr.length;
+
+//         for(int i=0; i<n; i++){
+//             for(int j=i+1; j<n; j++){
+//                 if(arr[i]+arr[j]==target){
+//                     System.out.println(arr[i]+ " " + arr[j]);
+//                 }
+//             }
+//         }
+//         System.out.println(target);
+//     }
+// }
