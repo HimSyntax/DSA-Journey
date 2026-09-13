49ece4a6d5b0860df2797b6e7ac4abc9c839d7f7
Himanshu Ranjan <btwhimanshuatwork@gmail.com>
2026-09-13 12:17:13 +0530

    Clean up commented rotation code in Striver.java
    Removed commented-out code for rotating a matrix by 90 degrees.

--- Striver.java
+++ Striver.java
@@ -1113,3 +1113,57 @@
 //         }
 //     }
 // }
+
+
+// Rotate matrix by 90 degrees //
+
+// class Striver {
+//     public static void main(String[] args) {
+        
+//         int[][]matrix = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+//         int n = matrix.length;
+//         int[][]ans = new int [n][n];
+
+//         for(int i=0; i<n; i++){
+//             for(int j=0; j<n; j++){
+//                 ans[j][n-1-i] = matrix[i][j];
+//             }
+//         }
+//         for(int i=0; i<n; i++){
+//             for(int j=0; j<n; j++){
+//                 System.out.print(ans[i][j] + " ");
+//             }
+//         }
+//         System.out.println();
+//     }
+// }
+
+
+// class Solution {  // OPTIMAL
+//     public void rotateMatrix(int[][] matrix) {
+
+//         int n = matrix.length;
+
+//         // Transpose
+//         for (int i = 0; i < n; i++) {
+//             for (int j = i + 1; j < n; j++) {
+//                 int temp = matrix[i][j];
+//                 matrix[i][j] = matrix[j][i];
+//                 matrix[j][i] = temp;
+//             }
+//         }
+//         // Reverse each row
+//         for (int i = 0; i < n; i++) {
+//             int left = 0;
+//             int right = n - 1;
+//             while (left < right) {
+//                 int temp = matrix[i][left];
+//                 matrix[i][left] = matrix[i][right];
+//                 matrix[i][right] = temp;
+
+//                 left++;
+//                 right--;
+//             }
+//         }
+//     }
+// }
